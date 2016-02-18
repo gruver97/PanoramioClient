cd7b8dd1770cdedb43f756a40f774cb9fa0913a3
gruver <gruver97@outlook.com>
2016-02-18 22:02:38 +0300

    Модель ответа сервера.

---
+++ PanoramioClient/PanoramioClient.Shared/Model/PhotoDescription.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace PanoramioClient.Model
+{
+    public class PhotoDescription
+    {
+        [JsonProperty("photo_id")]
+        public int PhotoId { get; set; }
+
+        [JsonProperty("photo_title")]
+        public string Title { get; set; }
+
+        [JsonProperty("photo_url")]
+        public string PhotoUrl { get; set; }
+
+        [JsonProperty("photo_file_url")]
+        public string PhotoFileUrl { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("width")]
+        public double Width { get; set; }
+
+        [JsonProperty("height")]
+        public double Height { get; set; }
+
+        [JsonProperty("upload_date")]
+        public double UploadDate { get; set; }
+
+        [JsonProperty("owner_id")]
+        public double OwnerId { get; set; }
+
+        [JsonProperty("owner_name")]
+        public double OwnerName { get; set; }
+
+        [JsonProperty("owner_url")]
+        public double OwnerUrl { get; set; }
+    }
+}
---
+++ PanoramioClient/PanoramioClient.Shared/Services/IPanoramioService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PanoramioClient.Services
+{
+    public interface IPanoramioService
+    {
+    }
+}
